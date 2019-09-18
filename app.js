3140af24317d0be7f554d9587d39fefe456bfa0b
Drew Powers <drew@pow.rs>
2019-09-18 15:05:58 -0600

    Add LogDNA

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello from Render!"));
+app.get('/', (req, res) => res.send('Hello from Render!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
