27be443c400444f432580f1e6966a24b146ff329
samrotella <48039441+samrotella@users.noreply.github.com>
2020-10-20 22:31:52 -0400

    adding a post

--- app.js
+++ app.js
@@ -4,4 +4,10 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.send("Goodbye From OneSpot!"));
 
+app.post('/captureEmail', function(request, response) {
+    return response.status(200).send(request.body.emailAddress);
+});
+
+module.exports = router;
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
