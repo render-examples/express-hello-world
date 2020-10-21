f996f39e6c159de2b803da4ff04250fb2a89da08
samrotella <48039441+samrotella@users.noreply.github.com>
2020-10-20 23:10:00 -0400

    adding body parser

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 
 app.get("/", (req, res) => res.send("Goodbye From OneSpot!"));
 
 app.post('/captureEmail', function(request, response) {
-    return response.status(200).send(request);
+    return response.sendStatus(200);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
