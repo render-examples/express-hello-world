580f1f6030d456111e87bf089a2da5588ec72f4b
Hieu <hieu@render.com>
2022-01-10 10:59:54 -0800

    test

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.send("Hello there 2!"));
+app.get("/envs", (req, res) => res.send(`SECRET: ${process.env['SECRET']}; SECRET_2: ${process.env['SECRET_2']}`));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
