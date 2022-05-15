f27285078f5a3d93e5c15fe570479f2af6cd8743
Vasanth Petlu <vasanth@virtualkitchen.co>
2022-05-14 18:05:07 -0700

    async await

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const Bull = require('bull');
 
 const myFirstQueue = new Bull('my-first-queue', 'redis://red-ca01lds6fj35fniee9ig:6379');
 
-const job = await myFirstQueue.add({
-  foo: 'bar'
-});
+app.listen(port, async () => {
+  console.log(`Example app listening YO on port ${port}!`)
 
-myFirstQueue.process(async (job) => {
-  return console.log(`the job ran! ${job.data}`);
-});
+  const job = await myFirstQueue.add({
+    foo: 'bar'
+  });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  myFirstQueue.process(async (job) => {
+    return console.log(`the job ran! ${job.data}`);
+  });
+
+});
