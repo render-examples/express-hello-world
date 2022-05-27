98eb9dc232ad23100e0e469caed98aad5acc8ce2
{AtushiFukuura} <{fukuura@odin.ne.jp}>
2022-05-27 21:52:36 +0900

    更新確認コミット

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/ by fukuura`);
 });
