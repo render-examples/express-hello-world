2a20f4f37a21072b0399017fe6cb9e6edcc609d5
Jonathan Gingras-Ouimet <jonathan.gingras@edu.cegepgarneau.ca>
2022-08-24 17:13:30 -0400

    commit

--- public/script.js
+++ public/script.js
@@ -6,7 +6,7 @@ myVideo.muted = true;
 var peer = new Peer(undefined, {
     path: '/peerjs',
     host: '/',
-    port: '3000'
+    port: '443'
 });
 
 let myVideoStream;
@@ -96,7 +96,7 @@ let text = $('input');
     });
 
     socket.on('createMessage', message => {
-        $('.messages').append(`<li class="message"><b>user</b><br />${message}</li>`)
+        $('ul').append(`<li class="message"><b>user</b><br />${message}</li>`)
         scrollToBottom();
     })
 
