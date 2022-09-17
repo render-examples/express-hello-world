e9f4d6059dd95c72ff6fe4be29fb663c19888d27
Hamza Ansari <ansarihamza919@gmail.com>
2022-09-17 19:36:29 +0530

    testing location

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const path = require('path')
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(port, () => {
