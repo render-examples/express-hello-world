513f9acf6b7ad6b2feea431536ca503a7c0585a1
Hamza Ansari <ansarihamza919@gmail.com>
2022-09-29 16:48:55 +0530

    video8

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const path = require('path')
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
