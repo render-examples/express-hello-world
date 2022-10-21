3ffab8ac2fc213f0baef8456eee1387037dd8fc7
aseruneko <aseruneko@gmail.com>
2022-10-22 01:08:23 +0900

    足し算機能

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
+app.post('/api/add', multipart.any(), function (req, res) {
+  const firstValue = parseInt(req.body.firstValue);
+  const secondValue = parseInt(req.body.secondValue);
+  const sum = firstValue + secondValue;
+  res.json({sum: sum, firstValue: firstValue, secondValue: secondValue});
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 
