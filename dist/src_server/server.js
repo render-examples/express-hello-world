29cf3f3d7af7b8c477a6eb7c54ef102728da2e25
aseruneko <aseruneko@gmail.com>
2022-10-22 00:40:03 +0900

    hoge

--- dist/src_server/server.js
+++ dist/src_server/server.js
@@ -10,7 +10,7 @@ const config = require('../../webpack.config.js');
 const app = express();
 const port = 8080;
 
-const devServerEnabled = true;
+const devServerEnabled = false;
 
 if (devServerEnabled) {
     config.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000');
@@ -25,6 +25,7 @@ if (devServerEnabled) {
 
     app.use(webpackHotMiddleware(compiler));
 }
+
 
 app.use(express.static('./public'));
 
