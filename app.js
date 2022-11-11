0145536c0c81317f8f48c6f79114ad2895222614
zeitseeing <zeitseeing@protonmail.com>
2022-11-11 07:08:19 +0100

    Read PORT from environment variable
    The port is hardcoded to 3001 or read from an environment variable.
    To be able to read the env variable this commit installs dotenv.
    
    https://www.npmjs.com/package/dotenv

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+require('dotenv').config();
+
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
