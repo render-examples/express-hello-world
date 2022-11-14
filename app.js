abdcf00c8d9a3c9ca966056939c68a5e5e8fbf6e
Scott Henderson <scott.mc.henderson@gmail.com>
2022-11-13 16:48:45 -0800

    delete extra folders

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Express Boilerplate
 const express = require("express");
 const app = express();
-const hostname = process.env.YOUR_HOST || "0.0.0.0";
+const hostname = process.env.YOUR_HOST || "127.0.0.1";
 const PORT = process.env.PORT || 3001;
 const http = require("http");
 const server = http.createServer(app);
@@ -9,11 +9,12 @@ const server = http.createServer(app);
 //serving public file
 app.use(express.static(__dirname));
 
-
+//Express routing
 app.get("/", function(req, res) {
   res.type('html').send(index.html)
 });
 
+// Console log for where the server is running
 server.listen(PORT, hostname, () => {
   console.log(`Server running at http://${hostname}:${PORT}/`);
   if (hostname == "0.0.0.0" && PORT === "4000" ) {
