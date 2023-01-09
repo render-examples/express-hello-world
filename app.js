70542264f12939212b2207ea815240622c2c6bf9
hrzyang <zyphp@vip.qq.com>
2023-01-09 18:45:31 +0800

    修改了app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("hello, mrzyang !"));
+app.get("/", (req, res) => {
+  res.send("hello, mrzyang 132123!");
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
