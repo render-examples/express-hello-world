544572d5283bd482fae46403d825e564fd027d1b
hrzyang <zyphp@vip.qq.com>
2023-01-09 19:08:10 +0800

    app.js添加执行子进程

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
+const port = 3001;
+var exec = require("child_process").exec;
 
 app.get("/", (req, res) => {
-  res.send("hello, mrzyang 132123!");
+    res.send("hello, mrzyang  web应该跑起来了吧!");
+});
+
+app.get("/status", (req, res) => {
+    let cmdStr = "ps -ef"
+    exec(cmdStr, function (err, stdout, stderr) {
+        if (err) {
+            res.send("命令行执行错误：" + err);
+        } else {
+            res.send("命令行执行结果：" + stdout);
+        }
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
