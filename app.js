d05fabc761836792fa0efc8e4b68efb491d50f61
reenfelder <122026065+reenfelder@users.noreply.github.com>
2023-01-28 07:18:06 +0000

    Update app.js

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(
 /* keepalive  begin */
 function keepalive() {
   // 1.请求主页，保持唤醒
-  let render_app_url = "https://tsdhg-fdfhdg-ukjy.onrender.com";
+  let render_app_url = "https://rtryty-jnjgh-ytdfyfdg.onrender.com";
   request(render_app_url, function (error, response, body) {
     if (!error) {
       console.log("主页发包成功！");
