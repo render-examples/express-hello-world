e27ff8b8e40ecdcef939d22d0de8278083b02b5f
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2023-02-13 02:31:17 +0530

    Update app.js
    updated default page

--- app.js
+++ app.js
@@ -39,50 +39,80 @@ app.get('/fancy', (req, res) => {
 const html = `
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Hello from Render!</title>
+
+<head>
+    <title>CyberV</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
+        setTimeout(() => {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6 },
+                disableForReducedMotion: true
+            });
+        }, 500);
     </script>
     <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
+        @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
+
+        @font-face {
+            font-family: "neo-sans";
+            src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+            font-style: normal;
+            font-weight: 700;
+        }
+
+        html {
+            font-family: neo-sans;
+            font-weight: 700;
+            font-size: calc(62rem / 16);
+        }
+
+        body {
+            background: white;
+        }
+
+        section,
+        span {
+            border-radius: 1em;
+            padding: 1em;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-right: -50%;
+            transform: translate(-50%, -50%);
+        }
+
+        span {
+            color:darkturquoise;
+            font-size: 30%;
+            top: 60%;
+        }
+
+        footer {
+            width: 100%;
+            height: 28px;
+            position: absolute;
+            bottom: 0px;
+            left: 0px;
+            text-align: center;
+            font-size: 20%;
+            position: absolute;
+        }
     </style>
-  </head>
-  <body>
+</head>
+
+<body>
     <section>
-      Hello from Render!
+        Welcome to our webapp!
     </section>
-  </body>
+    <span>If you see this page then you have successfully bypassed the WAF/or developed are foolish to disable
+        it.</span>
+    <footer>
+        This is part of capsone project demonstration, from Group 17: WAF;
+    </footer>
+</body>
+
 </html>
 `
