05e5bebee9cf0b4c27d6b98f8dac441e78118562
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2023-02-28 17:20:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 app.set('views', path.join(__dirname,"views"));
 app.set('view engine','ejs');
 //console.log(indexRoutes)
-var site = 'https://dummy-web-app.onrender.com';
 // Forward all requests to the WAF on port 3001
 app.use((req,res,next)=>{
   const appReq = request({
-    uri: site + req.url,
+    uri: 'http://localhost:3001' + req.url,
     headers: req.headers
   });
   req.pipe(appReq);
@@ -32,10 +31,7 @@ app.use((req,res,next)=>{
 
 app.use(indexRoutes)
 
-// // Start the server
-// in case using localhost uncomment the below code and comment the next one.
-// app.listen(3002, 'localhost', () => {
-//   console.log('Backend server listening on port 3002');
-// });
-
-app.listen(3002, () => console.log(`Backend server listening on port 3002`));
+// Start the server
+app.listen(3002, 'localhost', () => {
+  console.log('Backend server listening on port 3002');
+});
