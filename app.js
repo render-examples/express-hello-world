54cf0a844b8b4c9d228d4d0cbfa8aca1a8b11bea
Arpan Thomas <66914302+Arpanthomas1121@users.noreply.github.com>
2023-03-03 11:11:44 +0530

    Update app.js
    removing buggy sections

--- app.js
+++ app.js
@@ -9,25 +9,25 @@ app.set('views', path.join(__dirname,"views"));
 app.set('view engine','ejs');
 //console.log(indexRoutes)
 // Forward all requests to the WAF on port 3001
-app.use((req,res,next)=>{
-  const appReq = request({
-    uri: 'http://localhost:3001' + req.url,
-    headers: req.headers
-  });
-  req.pipe(appReq);
-  appReq.on('response', (appRes) => {
-    //console.log(appRes)
-    if(appRes.statusCode === 200 || appRes.statusCode === 304){
-      next();
-    }else{
-      res.status(appRes.statusCode).sendStatus(appRes.statusCode);
-      //router.get("*", (req, res) => {
-       // res.render('404page');
-     // })
-    }    
+// app.use((req,res,next)=>{
+//   const appReq = request({
+//     uri: 'http://localhost:3001' + req.url,
+//     headers: req.headers
+//   });
+//   req.pipe(appReq);
+//   appReq.on('response', (appRes) => {
+//     //console.log(appRes)
+//     if(appRes.statusCode === 200 || appRes.statusCode === 304){
+//  next();
+//     }else{
+//       res.status(appRes.statusCode).sendStatus(appRes.statusCode);
+//       //router.get("*", (req, res) => {
+//        // res.render('404page');
+//      // })
+//     }    
     
-  });
-});
+//   });
+// });
 
 app.use(indexRoutes)
 
