605a591e6db290926820e9bc7a50ff755f2a6704
Steven Smith <82333766+stevensmith0918one@users.noreply.github.com>
2023-06-08 08:23:49 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
+app.get('/healthz', (req, res) => {
+  res.sendStatus(200)
+})
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
