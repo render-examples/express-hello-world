3304d01df7a6e0dc6d5cd6523969d7688991f460
Ignacio <ignaciojsoler@gmail.com>
2023-07-31 19:29:55 -0300

    Fix: uploadFile path correction

--- src/helpers/uploadFile.ts
+++ src/helpers/uploadFile.ts
@@ -1,11 +1,14 @@
 import { Request } from 'express';
 import multer from 'multer';
+import path from 'path';
 import {v4 as uuidv4} from 'uuid';
+const uploadsPath = path.join(__dirname, "..", "uploads");
 
 const generateFileName = (req: Request, file: Express.Multer.File, callback: (error: Error | null, filename: string) => void) => {
   const fileFormat = file.mimetype.split('/').pop();
   const fileName = `${uuidv4()}.${fileFormat}`;
+  console.log(uploadsPath);
   callback(null, fileName);
 };
 
-export const upload = multer({ storage: multer.diskStorage({ destination: 'src/uploads/', filename: generateFileName }) });
+export const upload = multer({ storage: multer.diskStorage({ destination: uploadsPath, filename: generateFileName }) });
