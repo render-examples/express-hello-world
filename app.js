2ad083c24d0b5d20b53c08f3ac29052d0a994c38
Matthew Chan <mattam@gmail.com>
2023-10-07 13:34:19 -0600

    Initial checkin of the API to generate stickers from Huggingface

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
+require('dotenv').config({path: './.env'});
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 app.get("/", (req, res) => res.type('html').send(html));
+
+app.get('/sticker/:prompt', async function (req, res) {
+
+  var prompt = {
+    inputs: `sticker, stickers, ${req.params.prompt}`,
+    parameters: {
+      negative_prompt: 'blurry',
+    }
+  };
+
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/artificialguybr/StickersRedmond",
+    {
+        headers: { Authorization: `Bearer ${process.env.HUGGINGFACE_API_KEY}` },
+        method: "POST",
+        body: JSON.stringify(prompt),
+    }
+);
+
+result = await response.arrayBuffer();
+const img = Buffer.from(result).toString("base64");
+
+const imageHTML = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello from Render!</title>
+  </head>
+  <body>
+    <section>
+      <img src="data:image/png;base64, ${img}">
+    </section>
+  </body>
+</html>
+`
+res.send(imageHTML);
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
@@ -14,47 +53,10 @@ const html = `
 <html>
   <head>
     <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
   </head>
   <body>
     <section>
-      Hello from Render!
+      Kidechism API
     </section>
   </body>
 </html>
