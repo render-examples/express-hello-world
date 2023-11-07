55aa338ecc66ce5fc83b211a2acba187f5829bfa
natstarrynight <nat@sieve.page>
2023-11-06 19:21:17 -0800

    updated font

--- app.js
+++ app.js
@@ -28,13 +28,13 @@ const html = `
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
       @font-face {
-        font-family: "neo-sans";
+        font-family: "fira-sans";
         src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
         font-style: normal;
         font-weight: 700;
       }
       html {
-        font-family: neo-sans;
+        font-family: fira-sans;
         font-weight: 700;
         font-size: calc(62rem / 16);
       }
