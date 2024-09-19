c1540a8136518d3dbff2fa1320a70f76183e4702
JBratvold <mcbratjo@gmail.com>
2024-09-19 13:43:23 -0600

    Add env variables for db

--- app.js
+++ app.js
@@ -1,24 +1,57 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+const path = require('path'); 
 const app = express();
 const port = process.env.PORT || 3001;
-const path = require('path'); 
-
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.static(path.join(__dirname, 'public')));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 const router = express.Router(); 
 
-// Setup essential routes 
 router.get('/', function(req, res) { 
   res.sendFile(path.join(__dirname + '/views/index.html')); 
-  //__dirname : It will resolve to your project folder. 
 }); 
 
 router.get('/name-comparison', function(req, res) { 
   res.sendFile(path.join(__dirname + '/views/name-comparison.html')); 
-  //__dirname : It will resolve to your project folder. 
 }); 
+
+router.get('/form', function(req, res) { 
+  res.sendFile(path.join(__dirname + '/views/form.html')); 
+}); 
+
+app.post('/x', (req, res) => {
+  res.send(`Full name is:${req.body.fname} ${req.body.lname}.`);
+  console.log(req.body.fname);
+  const name = req.body.fname
+  
+  const db_url = process.env.DB_URL;
+  const db_auth_token = process.env.DB_AUTH_TOKEN;
+  // const query = "create table if not exists "+name+" (id integer primary key,title varchar(255));"
+  const query = "drop table movies;"
+
+
+  fetch(db_url, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${db_auth_token}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      requests: [
+        { type: "execute", stmt: { sql: query } },
+        { type: "close" },
+      ],
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+
+});
+
 
 router.use(function(req, res) {
   res.status(404).end('Error 404. Page is non-existant.');
