fc380d348c3fb35bd0312e8a2e09341a86689f60
taneralicantaner <alicantaner@hotmail.com>
2025-04-24 12:14:54 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+//app.get("/", (req, res) => res.type('html').send(html));
+
+app.get("/", (req, res) => {
+  setTimeout(() => {
+    return res.send('success');
+  }, "120000");
+});
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
