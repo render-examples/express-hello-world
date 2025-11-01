18aff9a3bcd134f979de89982f9fc88e81457a75
PraxisAssistKI <wissmannpascaladam@web.de>
2025-11-01 21:01:22 +0100

    PraxisAssist KI Voice Handler

--- app.js
+++ app.js
@@ -1,61 +1,34 @@
-const express = require("express");
+import express from "express";
+import twilio from "twilio";
+
 const app = express();
+
+// Twilio schickt Form-Daten bei einem Anruf (POST)
+app.use(express.urlencoded({ extended: true }));
+
+// Diese Route wird von Twilio angerufen, wenn jemand die Praxis anruft
+app.post("/", (req, res) => {
+  // TwiML = was Twilio dem Anrufer vorspielen / sagen soll
+  const twiml = new twilio.twiml.VoiceResponse();
+
+  // Ansage für eure Praxis
+  twiml.say(
+    {
+      voice: "Polly.Vicki",   // weibliche Stimme
+      language: "de-DE"       // deutsch
+    },
+    "Willkommen in der hausärztlichen Praxis Dr. Müller. "
+    + "Bitte nennen Sie kurz Ihren Namen, Ihr Anliegen, und ob es dringend ist. "
+    + "Wir rufen Sie schnellstmöglich zurück. Vielen Dank."
+  );
+
+  // Twilio erwartet XML als Antwort
+  res.type("text/xml");
+  res.send(twiml.toString());
+});
+
+// Server starten
 const port = process.env.PORT || 3001;
-
-app.get("/", (req, res) => res.type('html').send(html));
-
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.listen(port, () => {
+  console.log(`PraxisAssist-KI Server läuft auf Port ${port}`);
+});
