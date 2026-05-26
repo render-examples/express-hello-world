744a76e58d82a91a0cb74834211642d9fe6efd6a
honguyenminh <honguyenminh@outlook.com>
2026-05-26 15:23:47 +0700

    Update HTML title to my MSSV

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render!</title>
+    <title>Hello from 23520923 - Hồ Nguyên Minh!</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
